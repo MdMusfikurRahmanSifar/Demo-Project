3d0e5e8a41b7754fbce4b75348a925e3ba2e9170
MdMusfikurRahmanSifar <121184191+MdMusfikurRahmanSifar@users.noreply.github.com>
2025-12-16 07:43:48 +0000

    revert

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask
-from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)   # <-- enables /metrics
 
 @app.route("/")
 def hello():
